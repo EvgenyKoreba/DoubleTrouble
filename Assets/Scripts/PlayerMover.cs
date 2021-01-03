d3017b3fd173f0ada7501ff68665fa64b90c49c0
Evgeny <64806042+EvgenyKoreba@users.noreply.github.com>
2021-01-04 00:31:18 +0300

    PlayerMover

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -12,19 +12,23 @@ public class PlayerMover : MonoBehaviour
         strong
     }
 
-        [Header("Set in Inspector")]
-    [SerializeField] private float          speedMult;
-    [SerializeField] private JumpType       jumpType = JumpType.idle;
-    [SerializeField] private bool isJumping = false;
-    [SerializeField] private float          weakJumpButtonHoldingTime;
+        [Header("Set in Inspector: Move Options")]
+    [SerializeField] private float          speed;
+
+        [Header("Set in Inspector: Jump Options")]
+    [SerializeField] private KeyCode        jumpButton = KeyCode.Space;
     [SerializeField] private float          weakJumpForce;
     [SerializeField] private float          maxJumpForce;
+    [SerializeField] private float          maxWeakJumpButtonHoldingTime;
     [SerializeField] private float          strongJumpTimeScaling;
 
 
-        [Header("Set Dynamically")]
-    [SerializeField] private float jumpButtonHoldingTime;
-    private Rigidbody2D rigidBody;
+    [Header("Set Dynamically")]
+    [SerializeField] private JumpType       jumpType = JumpType.idle;
+    [SerializeField] private float          jumpButtonHoldingTime;
+
+
+    private Rigidbody2D                     rigidBody;
 
 
     private void Awake()
@@ -34,44 +38,37 @@ public class PlayerMover : MonoBehaviour
 
     private void Update()
     {
-        //if (Input.GetKeyDown(KeyCode.Q))
-        //{
-        //    rigidBody.AddForce(new Vector2(0, 500));
-        //}
-    }
-
-    private void FixedUpdate()
-    {
-        // Move
-        float x = Input.GetAxis("Horizontal");
-        float speed = x * speedMult * Time.fixedDeltaTime;
-
-        // Jump
-        if (Input.GetKeyDown(KeyCode.Space))
+        // Jump     
+        if (Input.GetKeyDown(jumpButton))
         {
             if (jumpType == JumpType.idle)
             {
                 jumpType = JumpType.weak;
-                isJumping = true;
             }
         }
-        else if (Input.GetKey(KeyCode.Space) && isJumping)
+
+        if (Input.GetKey(jumpButton))
         {
-            jumpButtonHoldingTime += Time.fixedDeltaTime;
-            if (jumpButtonHoldingTime > weakJumpButtonHoldingTime)
+            jumpButtonHoldingTime += Time.deltaTime;
+            if (jumpButtonHoldingTime > maxWeakJumpButtonHoldingTime)
             {
                 jumpType = JumpType.strong;
             }
         }
 
-        if (Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetKeyUp(jumpButton))
         {
-             Jump();
+            Jump();
+            jumpButtonHoldingTime = 0.0f;
         }
+    }
 
-        Vector3 newPosition = transform.position;
-        newPosition.x += speed;
-        transform.position = newPosition;
+
+    private void FixedUpdate()
+    {
+        // Move
+        float moveInput = Input.GetAxis("Horizontal");
+        rigidBody.velocity = new Vector2(moveInput * speed, rigidBody.velocity.y);
     }
 
 
@@ -82,9 +79,11 @@ public class PlayerMover : MonoBehaviour
         {
             case JumpType.weak:
                 jumpForce.y = weakJumpForce;
+                //Debug.Log("Weak jump");
                 break;
             case JumpType.strong:
                 jumpForce.y = Mathf.Min(maxJumpForce, weakJumpForce + jumpButtonHoldingTime * strongJumpTimeScaling);
+                //Debug.Log("Strong jump");
                 break;
         }
         rigidBody.AddForce(jumpForce);
@@ -92,11 +91,9 @@ public class PlayerMover : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (isJumping)
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
         {
             jumpType = JumpType.idle;
-            isJumping = false;
-            jumpButtonHoldingTime = 0;
         }
     }
 }
