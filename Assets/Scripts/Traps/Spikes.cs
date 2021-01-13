9864750ce7baa03e9e90401eaf0d0799fdd2067a
Skindrow <75908610+Skindrow@users.noreply.github.com>
2021-01-13 18:27:21 +0300

    Spikes
    промежуточный каммит , немног подправил шипы

--- Assets/Scripts/Traps/Spikes.cs
+++ Assets/Scripts/Traps/Spikes.cs
@@ -26,7 +26,7 @@ public class Spikes : MonoBehaviour
         }
         else if (type == TypeOfSpikes.Looping)
         {
-            StartCoroutine(LoopSpikes());
+            StartCoroutine(LoopSpikesMove());
         }
         else if (type == TypeOfSpikes.Triggered)
         {
@@ -46,35 +46,49 @@ public class Spikes : MonoBehaviour
     }
 
 
-    private IEnumerator LoopSpikes()
-    {
-        while (true)
-        {
-            StartCoroutine(PushSpikes());
-            yield return new WaitForSeconds(loopTime);
-            StartCoroutine(HideSpikes());
-            yield return new WaitForSeconds(activatingTime);
-
-        }
-    }
+    
     private IEnumerator PushSpikes()
     {
         while (spikes.position != spikesPushPosition.position)
         {
-            spikes.position = Vector2.Lerp(spikes.position, spikesPushPosition.position, 0.1f);
+            spikes.position = Vector2.MoveTowards(spikes.position, spikesPushPosition.position, 0.1f);
             yield return null;
         }
+
     }
     private IEnumerator HideSpikes()
     {
         while (spikes.position != startSpikesPos)
         {
-            spikes.position = Vector2.Lerp(spikes.position, startSpikesPos, 0.1f);
+            spikes.position = Vector2.MoveTowards(spikes.position, startSpikesPos, 0.1f);
             yield return null;
+        }
+    }
+    private IEnumerator LoopSpikesMove()
+    {
+        while (true)
+        {
+            while (spikes.position != spikesPushPosition.position)
+            {
+                spikes.position = Vector2.MoveTowards(spikes.position, spikesPushPosition.position, 0.1f);
+                yield return null;
+            }
+
+
+            yield return new WaitForSeconds(activatingTime);
+
+
+            while (spikes.position != startSpikesPos)
+            {
+                spikes.position = Vector2.MoveTowards(spikes.position, startSpikesPos, 0.1f);
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(loopTime);
         }
     }
     private void OnTriggerEnter(Collider other)
     {
-        
+
     }
 }
