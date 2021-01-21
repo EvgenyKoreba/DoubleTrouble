261146ba400ab933bfbf2273b563a7dfc5073c5a
Evgeny <64806042+EvgenyKoreba@users.noreply.github.com>
2021-01-22 00:16:18 +0300

    Checkpoints temp

--- Assets/Scripts/Prefabs/CheckPoint.cs
+++ Assets/Scripts/Prefabs/CheckPoint.cs
@@ -4,45 +4,59 @@ using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
 {
+
+    private enum CheckpointType
+    {
+        Intermediate,
+        Final
+    }
+
+
     [Header("Set Dynamically")]
-    [SerializeField] private bool       isAchived = false;
+    [SerializeField] private bool               isReached = false;
+    [SerializeField] private CheckpointType     type = CheckpointType.Intermediate;
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (isReached)
+        {
+            return;
+        }
+
         Player player = collision.gameObject.GetComponent<Player>();
-        if (player != null && !isAchived)
-            Achiev(player);
+        if (player != null)
+            Reached(player);
     }
 
 
-    private void Achiev(Player player)
+    private void Reached(Player player)
     {
-        isAchived = true;
+        isReached = true;
         player.SetRespawnPos(transform.position);
+        GUI.Button(new Rect(10, 10, 150, 150), "I am a button");
     }
 
 
-    //private void Start()
-    //{
-    //    EventManager.PostNotification(EVENT_TYPE.CheckpointReached, new GameObject());
-    //    EventManager.PostNotification(EVENT_TYPE.CheckpointReached);
-    //}
+
+    private void OnEnable()
+    {
+        EventManager.Subscribe(EVENT_TYPE.CheckpointReached, Test);
+    }
 
 
-    //private void OnEnable()
-    //{
-    //    EventManager.Subscribe(EVENT_TYPE.CheckpointReached, Test);
-    //}
+    private void OnDisable()
+    {
+        EventManager.Unsubscribe(EVENT_TYPE.CheckpointReached, Test);
+    }
 
+    private void Test(object[] pars)
+    {
 
-    //private void OnDisable()
-    //{
-    //    EventManager.Unsubscribe(EVENT_TYPE.CheckpointReached, Test);
-    //}
+    }
 
-    //private void Test(object[] pars)
-    //{
-
-    //}
+    private void OnTriggerEnter(Collider other)
+    {
+        EventManager.PostNotification(EVENT_TYPE.CheckpointReached, this);
+    }
 }
---
+++ Assets/Scripts/UI_InGameDrawer/UI_InsertsContainer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_InsertsContainer : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    [SerializeField] private Canvas checkpointReach;
+    [SerializeField] private Canvas levelFinish;
+
+
+    private static UI_InsertsContainer _S;
+
+    static public UI_InsertsContainer S
+    {
+        get { return _S; }
+        private set { _S = value; }
+    }
+
+
+    private void Awake()
+    {
+        if (S == null)
+        {
+            S = this;
+        }
+        else
+        {
+            Destroy(S.gameObject);
+            S = this;
+        }
+
+        DontDestroyOnLoad(S.gameObject);
+    }
+}
