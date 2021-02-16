6227c001ac3af4d1e8021e54c6ae4e2bdeb3896e
Skindrow <Skindrow00@gmail.com>
2021-02-16 17:16:40 +0300

    Gen start

---
+++ Assets/Scripts/MapGen/Tile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] Sprite[] squareSpritesSet;
+    [SerializeField] TypeOfTile typeOfTile;
+    void Start()
+    {
+        int rnd = Random.Range(0, squareSpritesSet.Length);
+        gameObject.GetComponent<SpriteRenderer>().sprite = squareSpritesSet[rnd];
+    }
+    public enum TypeOfTile
+    {
+        Square,
+        AngledLeft,
+        AngledRight
+    }
+
+}
