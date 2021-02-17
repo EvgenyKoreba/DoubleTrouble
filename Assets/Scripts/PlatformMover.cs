8b6bf65ec3e5ae3c94b528c9ec82adee7be720f9
Skindrow <Skindrow00@gmail.com>
2021-02-17 13:35:10 +0300

    PlatformMover 0.1
    все это дело не оч удобно т.к. скрипт движения платформы вешается на каждый тайл в платформе.

---
+++ Assets/Scripts/PlatformMover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMover : MovingBehaviour
+{
+    private void Start()
+    {
+        Move();
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Player>() != null || collision.gameObject.GetComponent<Item>() != null)
+        {
+            collision.gameObject.transform.parent = gameObject.transform;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Player>() != null || collision.gameObject.GetComponent<Item>() != null)
+        {
+            collision.gameObject.transform.parent = null;
+        }
+    }
+}
