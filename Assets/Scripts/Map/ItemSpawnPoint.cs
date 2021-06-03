88e54e32c2241e390ea8f6234b31eefdd0dfbaa0
Skindrow <75908610+Skindrow@users.noreply.github.com>
2021-06-03 19:39:28 +0300

    Huita

---
+++ Assets/Scripts/Map/ItemSpawnPoint.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawnPoint : MonoBehaviour
+{
+    [SerializeField] private GameObject[] itemsPool;
+    [SerializeField] private float[] persOfSpawnItem;
+    [Header("pers of any spawn from 0 to 100")]
+    [SerializeField] private float persOfSpawn;
+
+
+
+    private void Start()
+    {
+        if (IsAnySpawn())
+            Instantiate(ItemRoll(), transform.position, Quaternion.identity);
+    }
+    private bool IsAnySpawn()
+    {
+        float rnd = Random.Range(0f, 100f);
+        if (rnd > persOfSpawn)
+            return false;
+
+        return true;
+    }
+    private GameObject ItemRoll()
+    {
+        float sumOfPers = 0f;
+        for (int i = 0; i < persOfSpawnItem.Length; i++)
+        {
+            sumOfPers += persOfSpawnItem[i];
+        }
+        float rnd = Random.Range(0f, sumOfPers);
+        for (int i = 0; i < persOfSpawnItem.Length; i++)
+        {
+            rnd -= persOfSpawnItem[i];
+            if (rnd <= 0)
+                return itemsPool[i];
+        }
+        print("itemRollError");
+        return null;
+    }
+}
--- Assets/Scripts/Map/ItemsDeliveryPoint.cs
+++ Assets/Scripts/Map/ItemsDeliveryPoint.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class ItemsDeliveryPoint : MonoBehaviour
 {
+    private List<Item> itemsCollected;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.GetComponent<Item>() != null)
         {
-            ItemTake(collision.gameObject);
+            ItemTake(collision.gameObject.GetComponent<Item>());
         }
     }
-    private void ItemTake( GameObject item )
+    private void ItemTake(Item item)
     {
-
+        itemsCollected.Add(item);
 
 
         Destroy(item);
